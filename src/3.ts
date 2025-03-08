4c624d46ddf7ec131dd2f60530b15ff81d6f4044
dzakirubik19 <dzakirubik19@gmail.com>
2025-03-08 20:42:16 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,10 @@
+interface Student {
+  name: string;
+  age: number;
+}
+
+const students: Student[] = [
+  { name: "Alice", age: 10 },
+  { name: "Bob", age: 12 },
+  { name: "Charlie", age: 15 },
+];
