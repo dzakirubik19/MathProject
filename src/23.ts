d0967eb662a69667e5c4bdf1f07a1fcf20a6b4cf
dzakirubik19 <dzakirubik19@gmail.com>
2025-04-01 16:12:56 +0000

    Add 23.ts

---
+++ src/23.ts
@@ -0,0 +1,16 @@
+// Example of a simple counter that increments each time it is called
+let count = 0;
+
+function incrementCounter() {
+    count++;
+}
+
+incrementCounter();
+incrementCounter();
+
+console.log(count); // Output: 1
+
+incrementCounter();
+incrementCounter();
+
+console.log(count); // Output: 2
